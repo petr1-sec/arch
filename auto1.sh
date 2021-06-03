25221cd2a8c694fb0857a53b9b16856d8442fb55
Пётр Первый ⟁ <72628056+petr1-sec@users.noreply.github.com>
2021-06-04 02:03:55 +0300

    Update auto1.sh

--- auto1.sh
+++ auto1.sh
@@ -63,4 +63,4 @@ pacstrap /mnt base base-devel linux linux-firmware nano dhcpcd netctl
 echo 'Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL 123)"
+arch-chroot /mnt sh -c "$(curl -fsSL https://raw.githubusercontent.com/petr1-sec/arch/main/auto2.sh)"
